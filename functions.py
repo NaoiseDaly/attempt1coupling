8233370cc7676af76100751b2af7de8f34eae3e7
Naoise <naoisedaly101@gmail.com>
2025-01-18 13:26:19 +0000

    implement a basic maximal coupling algo and funs for visuals

--- functions.py
+++ functions.py
@@ -1,6 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import logging
 logger = logging.getLogger(__name__)
 from time import perf_counter
-from scipy.stats import norm
+from scipy.stats import norm, uniform
+
+
+
+
+def max_coupling_algo1(log_p_pdf, log_q_pdf, p_sampler, q_sampler):
+    """
+    Sampling from a maximal coupling of x ~ p and y ~ q, using Algorithm 1 from P.Jacob 2021
+    
+    """
+    new_X = p_sampler()
+    u  = uniform.rvs()
+    if np.log(u) + log_p_pdf(new_X) <= log_q_pdf(new_X):
+        return (new_X, new_X) # X=Y
+    
+    new_Y = None
+    while not new_Y:
+        proposed_Y = q_sampler()
+        u  = uniform.rvs()
+        if np.log(u) + log_q_pdf(proposed_Y) > log_p_pdf(proposed_Y):
+            new_Y = proposed_Y
+
+    return (new_X, new_Y)
+
+
+def just_plot_it(x, y,  title = None ):
+
+    fig, ax = plt.subplots()
+
+    ax.scatter(x,y)
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    if title:
+        plt.suptitle(title)
+    plt.show()
+
+def plot_joint_marginal(df, x_label = "X", y_label = "Y"):
+    z = sns.JointGrid(df, x = x_label, y = y_label)
+    z.plot_joint(sns.scatterplot)
+    z.plot_marginals(sns.histplot, kde = True, color = "r")
+    plt.show()
--- running.py
+++ running.py
@@ -1,5 +1,17 @@
 from functions import *
 import logging 
+import pandas as pd
 logger = logging.getLogger(__name__)
 logging.basicConfig( level=logging.INFO)
+from scipy.stats import norm
+np.random.seed(42421)
 
+sample = [
+    max_coupling_algo1(norm(10,1).logpdf, norm(0,1).logpdf, norm(10,1).rvs, norm(0,1).rvs)
+    for _ in range(200)]
+df = pd.DataFrame({"X":[pair[0] for pair in sample],
+                   "Y":[ pair[1] for pair in sample]})
+
+
+# just_plot_it(x_sample, y_sample)
+plot_joint_marginal(df)
