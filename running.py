1d216e2f4a9111d6c21ac8288aaec4b7908f401c
Naoise <naoisedaly101@gmail.com>
2025-01-19 15:32:06 +0000

    demo cMCMC with burnin

--- running.py
+++ running.py
@@ -4,19 +4,26 @@ import pandas as pd
 logger = logging.getLogger(__name__)
 logging.basicConfig( level=logging.INFO)
 from scipy.stats import norm
-np.random.seed(101)
+np.random.seed(1345301)
 
-# sample = [
-#     max_coupling_algo1(norm(10,1).logpdf, norm(0,1).logpdf, norm(10,1).rvs, norm(0,1).rvs)
-#     for _ in range(200)]
-# df = pd.DataFrame({"X":[pair[0] for pair in sample],
-#                    "Y":[ pair[1] for pair in sample]})
-
-
-# just_plot_it(x_sample, y_sample)
-# plot_joint_marginal(df)
+"""looking at the maximal coupling algorithm on its own"""
+sample = [
+    max_coupling_algo1(norm(10,1).logpdf, norm(0,1).logpdf, norm(10,1).rvs, norm(0,1).rvs)
+    for _ in range(200)]
+df = pd.DataFrame({"X":[pair[0] for pair in sample],
+                   "Y":[ pair[1] for pair in sample]})
 
 
-sample = coupled_MCMC1(30)
-# print(sample)
+just_plot_it(df["X"], df["Y"])
+plot_joint_marginal(df)
+
+"""Doing MCMC with the maximal coupling algorithm as the proposal dist"""
+N = 2000
+sample = coupled_MCMC1(N)
+sample_after_burn_in = sample.iloc[N//5:]
+
+print_basic_df_summary(sample)
+print_basic_df_summary(sample_after_burn_in)
+
 plot_joint_marginal(sample)
+plot_joint_marginal(sample_after_burn_in)
