9b25cb687ca5b6edb52cddec1ae828efc0e79d44
Naoise <naoisedaly101@gmail.com>
2025-01-28 15:49:23 +0000

    add more ways of looking at tau_lag

--- running.py
+++ running.py
@@ -47,9 +47,9 @@ N, L = 600, 200
 # print_basic_df_summary(c_sample.iloc[((N-L)//5)+L:])
 
 #plot TV upper bound
-#  
-tv_est = read_df_file("TV est2025-01-27 Mon 17-18.csv")
-print(tv_est)
+
+tv_f = "TV est 2025-01-28 Tue 15-06.csv"#"tv est 2025-01-28 Tue 10-20.csv" 
+tv_est = read_df_file(tv_f)
 
 fig , ax1 = plt.subplots(1)
 ax1.plot(tv_est)
@@ -59,14 +59,24 @@ ax1.set_ylabel("TV upper bound")
 ax1.set_xlabel("time t")
 plt.show()
 
-d = pd.read_csv(os.path.join("logs_and_data","tau lag 2025-01-27 Mon 20-19.csv") )
+# d = pd.read_csv(os.path.join("logs_and_data","tau lag 2025-01-27 Mon 20-19.csv") )
+tau_f = "tau lag 2025-01-28 Tue 15-06.csv" #"tau lag 2025-01-28 Tue 10-20.csv" # 
+d = read_df_file(tau_f)
+
+fig , (ax1) = plt.subplots(1)
+for c in d.columns:
+    xs = d[c] -int(c) #tau - lag
+    ax1.ecdf(xs, complementary  = True )
+
+ax1.legend(d.columns, title = "lag")
+ax1.set_yscale("log")
+plt.show()
+
 shifted = d.apply(lambda s:s -int(s.name), axis = 0)
 print_basic_df_summary(shifted)
-# fig , (ax1, ax2) = plt.subplots(2)
-# for c in d.columns:
-#     xs = d[c] -int(c) #tau - lag
-#     ax1.ecdf(xs, complementary  = False )
-#     ax2.boxplot(xs)
-# ax1.legend(d.columns, title = "lag")
-# ax1.set_yscale("log")
-# plt.show()
+fig , (ax1, ax2) = plt.subplots(1,2)
+ax1.boxplot(d, tick_labels = d.columns)
+ax1.set_title("tau")
+ax2.boxplot(shifted, tick_labels = d.columns)
+ax2.set_title("tau-lag")
+plt.show(	)
