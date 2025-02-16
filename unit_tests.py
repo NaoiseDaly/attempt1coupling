419e2674d2812876522d449e938f73fcdf417aea
Naoise <121348631@umail.ucc.ie>
2025-02-16 14:54:55 +0000

    refactor unit_test code

--- unit_tests.py
+++ unit_tests.py
@@ -1,6 +1,6 @@
 from functions import read_df_file
 import os, logging
-from generate_tau_samples import sample_tau_L_for_many_lags, mcmc3, mcmc4
+from generate_tau_samples import *
 log_path = os.path.join("logs_and_data", "MCMCcouplingSimulation.log")#os safe
 logging.basicConfig(filename = log_path , level=logging.INFO)
 test_logger = logging.getLogger(__name__)
@@ -9,16 +9,21 @@ FOLDER_PATH = os.path.join("keep_safe", "unit_test_data")
 
 
 
-def check_reproducability_sample_tau_L_for_many_lags():
+def reproduce__sample_tau_L_for_many_lags(algo):
+    """
+    check reproducability by running `algo` with hardcoded inputs and seed, and 
+    then comparing to prior saved output of tau.
 
+    For simplicity every `algo` has been executed on the same lags and seed
+    """
     #run the simulation now using `starting_random_seed` = 10101010
     current_answer = sample_tau_L_for_many_lags(
         mcmc4,
         lags = [300, 500, 800], 
-        num_tau_samples = 10_000, starting_random_seed= 10101010)
+        num_tau_samples = 100, starting_random_seed= 10101010)
 
     #load result carried out before using `starting_random_seed` = 10101010
-    f_name = "check_reproducability_sample_tau_L_for_many_lags3.csv"
+    f_name = f"check_reproducability_sample_tau_L_for_many_lags__{algo.__name}.csv"
 
     original_answer = read_df_file(f_name, FOLDER_PATH)
     # nice little catch that would make a df comparison fail
@@ -35,15 +40,18 @@ def run_all_checks():
     
     Please ensure this is nested somehow in an `if __name__ = "__main__"`"""
     test_logger.info("\t starting unit tests")
-    for test in [
-        check_reproducability_sample_tau_L_for_many_lags
-        ]:
-        try:
-            test()
-        except AssertionError as e:
-            msg = f"unit test {test.__name__} failed"
-            test_logger.error(msg)
-            print(msg)
+    tests = [
+        reproduce__sample_tau_L_for_many_lags
+        ]
+    funcs = [modified_coupled_MCMC2, mvn_2d_mcmc]
+    for test in tests:
+        for func in funcs:
+            try:
+                test(func)
+            except AssertionError as e:
+                msg = f"{func.__name} failed unit test {test.__name__} "
+                test_logger.error(msg)
+                print(msg)
 
     test_logger.info("\t finished unit tests \n")
 
