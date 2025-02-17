ca54c33d80b085aa3b8a0d6f100ed35b9ef14a71
Naoise <121348631@umail.ucc.ie>
2025-02-17 19:26:21 +0000

    uncomment code

--- create_unit_test_data.py
+++ create_unit_test_data.py
@@ -13,21 +13,21 @@ if __name__ == "__main__":
     remote_logger.info("\n") # add a line to seperate this execution from any others
     
     #original mcmc algorithms
-    # for algo in [modified_coupled_MCMC2, mvn_2d_mcmc, mcmc4]:
-    #     remote_logger.info(f"running {algo.__name__}")
-    #     tau_data = sample_tau_L_for_many_lags(
-    #         algo,
-    #         lags = [300, 500, 800],
-    #         num_tau_samples = 100,
-    #         starting_random_seed =10101010
-    #         )   
+    for algo in [modified_coupled_MCMC2, mvn_2d_mcmc, mcmc4]:
+        remote_logger.info(f"running {algo.__name__}")
+        tau_data = sample_tau_L_for_many_lags(
+            algo,
+            lags = [300, 500, 800],
+            num_tau_samples = 100,
+            starting_random_seed =10101010
+            )   
         
-    #     f_name = f"check_reproducability_sample_tau_L_for_many_lags__{algo.__name__}.csv"
-    #     f_path = os.path.join("logs_and_data", f_name)
+        f_name = f"check_reproducability_sample_tau_L_for_many_lags__{algo.__name__}.csv"
+        f_path = os.path.join("logs_and_data", f_name)
 
-    #     tau_data.to_csv(f_path, index = True)
+        tau_data.to_csv(f_path, index = True)
 
-    #     remote_logger.info(f"saved to {f_path}")
+        remote_logger.info(f"saved to {f_path}")
     
     #covariance matrices
     p = 10; num_matrices = 10
