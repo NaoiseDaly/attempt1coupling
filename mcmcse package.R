7b5305649c4f15d964d39fa2dc30be7c6d755614
Naoise <121348631@umail.ucc.ie>
2025-03-05 13:39:34 +0000

    add ess analysis of auto cor target

--- mcmcse package.R
+++ mcmcse package.R
@@ -1,4 +1,5 @@
 rm(list = ls())
+dev.off()
 require("mcmcse")
 # setwd(
 #   r"(C:\Users\Naoise Daly\OneDrive - University College Cork\FYP\attempt1coupling\keep_safe\3D-MVN-sample)"
@@ -7,9 +8,15 @@ setwd(
     r"(C:\Users\Naoise Daly\OneDrive - University College Cork\FYP\attempt1coupling\logs_and_data)"
    )
 
-B = 518
-l.chain <- read.csv("high_autocorrelated_mvn-P2-Seed42_lagged_chain.csv", header = F)
-nl.chain<- read.csv("high_autocorrelated_mvn-P2-Seed42_unlagged_chain.csv", header = F)
+B = 940#518 #read it from log file
+# l.chain <- read.csv("high_autocorrelated_mvn-P3-Seed42_lagged_chain.csv"
+#                     , header = F)
+# nl.chain<- read.csv("high_autocorrelated_mvn-P3-Seed42_unlagged_chain.csv"
+#                     , header = F)
+l.chain <- read.csv("high_autocorrelated_mvn-P3-Seed42_lagged_chain_2025-03-05 Wed 13-01.csv"
+                    , header = F)
+nl.chain<- read.csv("high_autocorrelated_mvn-P3-Seed42_unlagged_chain2025-03-05 Wed 13-01.csv"
+                    , header = F)
 l.chain <- l.chain[B:dim(l.chain)[1], ]
 nl.chain <- nl.chain[B:dim(nl.chain)[1], ]
 
@@ -26,9 +33,8 @@ for (c in 1:ncol(l.chain)){
        )
 }
 
-
-multiESS(l.chain); nrow(l.chain)
-multiESS(nl.chain); nrow(nl.chain)
+nrow(l.chain);multiESS(l.chain);ess(l.chain)
+nrow(nl.chain);multiESS(nl.chain);ess(nl.chain)
 
 
 
