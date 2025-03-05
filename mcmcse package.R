ccbf502f187985e596c826d4bd438e5c87691956
Naoise <121348631@umail.ucc.ie>
2025-03-05 11:00:55 +0000

    using for new data

--- mcmcse package.R
+++ mcmcse package.R
@@ -1,11 +1,15 @@
 rm(list = ls())
 require("mcmcse")
+# setwd(
+#   r"(C:\Users\Naoise Daly\OneDrive - University College Cork\FYP\attempt1coupling\keep_safe\3D-MVN-sample)"
+# )
 setwd(
-  r"(C:\Users\Naoise Daly\OneDrive - University College Cork\FYP\attempt1coupling\keep_safe\3D-MVN-sample)"
-)
+    r"(C:\Users\Naoise Daly\OneDrive - University College Cork\FYP\attempt1coupling\logs_and_data)"
+   )
+
 B = 518
-l.chain <- read.csv("lagged-chain.csv", header = F)
-nl.chain<- read.csv("nonlagged-chain.csv", header = F)
+l.chain <- read.csv("high_autocorrelated_mvn-P2-Seed42_lagged_chain.csv", header = F)
+nl.chain<- read.csv("high_autocorrelated_mvn-P2-Seed42_unlagged_chain.csv", header = F)
 l.chain <- l.chain[B:dim(l.chain)[1], ]
 nl.chain <- nl.chain[B:dim(nl.chain)[1], ]
 
